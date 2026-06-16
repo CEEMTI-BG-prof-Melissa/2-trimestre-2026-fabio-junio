43650138d59b41ee14861fd12d078c83d06f2f4b
Fabio junio Agostinho Silva <fabiojunio.asilva@aluno.edu.es.gov.br>
2026-06-16 11:56:38 -0300

    index.php

---
+++ aula01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title> Aula 01 </title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Olá,<br> Mundo!</h1>";
+        ?>
+    </body>
+</html>
