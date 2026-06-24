714f67175b33ccca987ef8d03bdd30103afc86f0
Fabio junio Agostinho Silva <fabiojunio.asilva@aluno.edu.es.gov.br>
2026-06-24 11:39:01 -0300

    index.php

---
+++ AULASPHaula02phph/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title> variaveisphp </title>
+    </head>
+    <body>
+    <?php
+    $nome = "fabio";
+    $idade = "15";
+    echo "meu nome é" .$nome. "e tenho" .$idade. "anos";
+    ?>
+    </body>
+</html>
