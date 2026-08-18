5e0896e36ea183853d1d442f5554fe4bab37fbfa
Fabio junio Agostinho Silva <fabiojunio.asilva@aluno.edu.es.gov.br>
2026-08-18 11:35:32 -0300

    index.php
    aula03.php

---
+++ Aula03/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Operações Aritiméticas</title>
+    </head>
+    <body>  
+<?php
+$preco = 120;
+$quantidade = 3;
+$desconto = 30;
+$parcelas = 2;
+$um = $preco * $quantidade;
+$dois = $um - $desconto;
+$tres = $dois / $parcelas;
+$quatro = $dois + 15;
+$cinco = $dois % 2;
+echo "Valor total da compra: R$ $um <br>";
+echo "Valor após o desconto: R$ $dois <br>";
+echo "Valor de cada parcela: R$ $tres <br>";
+echo "Valor com taxa de 15: R$ $quatro <br>";
+echo "Resto da divisão por 2: $cinco <br>";
+?>
+    ?>
+    </body>
+</html>
